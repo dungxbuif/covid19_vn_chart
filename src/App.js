1d13588417b3a4373d87c2d99b6a447d937179b8
dungxbuif <49265862+dungxbuif@users.noreply.github.com>
2021-06-14 17:07:40 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { getCountries, getReportByCountry } from "./apis";
+import CountrySelector from "./components/CountrySelector";
+import HighLight from "./components/HighLight";
+import Summary from "./components/Summary";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [countries, setCountries] = useState([]);
+  const [selectedCountryID,setSelectedCountryID] = useState('');
+  const [report, setReport] =useState([]);
+  const handleOnChange = (e) => {
+    setSelectedCountryID(e.target.value);
+  };
+
+  useEffect(()=>{ 
+    getCountries().then(res =>{
+        setCountries(res.data);
+        setSelectedCountryID('vn');
+    })
+  }, []);
+
+  useEffect(() => {
+    if(selectedCountryID){
+      const { slug } = countries.find(
+        country => country.ISO2.toLowerCase() === selectedCountryID
+      );
+      
+      getReportByCountry(slug)
+        .then(res => setReport(res.data));
+    }
+  },[countries,selectedCountryID]);
+
+  
+  return <>
+    <CountrySelector countries={countries} handleOnChange={handleOnChange} />
+    <HighLight report={report} />
+    {/* <Summary report={report} /> */}
+  </>;
 }
 
 export default App;
