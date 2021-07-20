0a26d9e1a593fba1df0b26c6587e7ad441ae1ac4
DungxBuif <dungbui.dungbui.00@gmail.com>
2021-07-20 20:51:47 +0700

    #update new API for Detail VN

--- crawl/moh.js
+++ crawl/moh.js
@@ -4,112 +4,168 @@ const { model } = require("mongoose");
 const cheerio = require("cheerio");
 const chalk = require("chalk");
 const fs = require("fs");
+const { resolve } = require("path");
 
 const successAlert = chalk.bold.cyan;
 const errorWaring = chalk.bold.red;
 
-module.exports = {
-    getMohAPI: async () => {
-        try {
-            const data = {};
-            const res = await axios.default({
-                method: "GET",
-                url: "http://ncov.moh.gov.vn/",
-                httpsAgent: new (require("https").Agent)({
-                    rejectUnauthorized: false,
-                }),
-            });
+const getLocalVN = async () => {
+    try {
+        let res = await axios.default({
+            method: "GET",
+            url: "http://ncov.moh.gov.vn/",
+            httpsAgent: new (require("https").Agent)({
+                rejectUnauthorized: false,
+            }),
+        });
 
-            console.log(
-                successAlert(
-                    `Get source successfully at ${moment(new Date()).format(
-                        "DD/MM/YYYY HH:mm:SS"
-                    )}`
-                )
-            );
+        console.log(
+            successAlert(
+                `Get MOH source successfully at ${moment(new Date()).format(
+                    "DD/MM/YYYY HH:mm:SS"
+                )}`
+            )
+        );
 
-            const $ = cheerio.load(res.data);
+        let $ = cheerio.load(res.data);
 
-            const selectorVN =
-                "#portlet_corona_trangchu_top_CoronaTrangchuTopPortlet_INSTANCE_RrVAbIFIPL7v > div > div.portlet-content-container > div > section.container > div.row.d-none.d-block.d-lg-none > div > div.form-row";
-            const selectorWorld =
-                "#portlet_corona_trangchu_top_CoronaTrangchuTopPortlet_INSTANCE_RrVAbIFIPL7v > div > div.portlet-content-container > div > section.container > div.row.d-none.d-block.d-lg-none > div > div.row";
-            const script = $("script:not([src],[type])");
-            let script_data = "";
-            for (let i = 0; i < script.length; i++) {
-                if (
-                    script[i].children[0].data.includes(
-                        'var data = [{"socakhoi"'
-                    )
-                ) {
-                    script_data = script[i].children[0].data;
-                    break;
-                }
+        const script = $("script:not([src],[type])");
+        let script_data = "";
+        for (let i = 0; i < script.length; i++) {
+            if (
+                script[i].children[0].data.includes('var data = [{"socakhoi"')
+            ) {
+                script_data = script[i].children[0].data;
+                break;
             }
-            const detail_vn = JSON.parse(script_data.match(/\[{.*?\}]/)[0]);
-            data.local_vn = {
-                Date: moment().format("DD-MM-YYYY"),
-                data: detail_vn,
-            };
-
-            $(selectorVN).each((index, el) => {
-                let Confirmed = $(el).find(".text-danger-new span").text();
-                let Active = $(el)
-                    .find(".text-warning1:contains(Đang điều trị) span")
-                    .text();
-                let Recovered = $(el)
-                    .find(".text-success:contains(Khỏi) span")
-                    .text();
-                let Deaths = $(el)
-                    .find(".text-danger-new1:contains(Tử vong) span")
-                    .text();
-                let dataVn = {
-                    Confirmed: Confirmed.split(".").join(""),
-                    Active: Active.split(".").join(""),
-                    Recovered: Recovered.split(".").join(""),
-                    Deaths: Deaths.split(".").join(""),
-                    Country: "Việt Nam",
-                    Slug: "vietnam",
-                    ISO2: "vn",
-                    Date: moment().format("DD-MM-YYYY"),
-                };
-
-                data.vietnam = dataVn;
-            });
-
-            $(selectorWorld).each((index, el) => {
-                let Confirmed = $(el).find(".text-danger-new span").text();
-                let Active = $(el)
-                    .find(".text-warning1:contains(Đang nhiễm) span")
-                    .text();
-                let Recovered = $(el)
-                    .find(".text-success:contains(Khỏi) span")
-                    .text();
-                let Deaths = $(el)
-                    .find(".text-danger-new1:contains(Tử vong) span")
-                    .text();
-                let dataWolrd = {
-                    Confirmed: Confirmed.split(".").join(""),
-                    Active: Active.split(".").join(""),
-                    Recovered: Recovered.split(".").join(""),
-                    Deaths: Deaths.split(".").join(""),
-                    Country: "Thế giới",
-                    Slug: "world",
-                    ISO2: "wld",
-                    Date: moment().format("DD-MM-YYYY"),
-                };
-                data.world = dataWolrd;
-            });
-
-            return data;
-        } catch (err) {
-            console.log(
-                errorWaring(
-                    `Get source fail because ${err} at ${moment(
-                        new Date()
-                    ).format("DD/MM/YYYY HH:MM:SS")}`
-                )
-            );
         }
-    },
+        const detail_vn = JSON.parse(script_data.match(/\[{.*?\}]/)[0]);
+        let local_vn = {
+            Date: moment().format("DD-MM-YYYY"),
+            data: detail_vn,
+        };
+        // const selectorVN =
+        //     "#portlet_corona_trangchu_top_CoronaTrangchuTopPortlet_INSTANCE_RrVAbIFIPL7v > div > div.portlet-content-container > div > section.container > div.row.d-none.d-block.d-lg-none > div > div.form-row";
+        // const selectorWorld =
+        //     "#portlet_corona_trangchu_top_CoronaTrangchuTopPortlet_INSTANCE_RrVAbIFIPL7v > div > div.portlet-content-container > div > section.container > div.row.d-none.d-block.d-lg-none > div > div.row";
+        // $(selectorVN).each((index, el) => {
+        //     let Confirmed = $(el).find(".text-danger-new span").text();
+        //     let Active = $(el)
+        //         .find(".text-warning1:contains(Đang điều trị) span")
+        //         .text();
+        //     let Recovered = $(el)
+        //         .find(".text-success:contains(Khỏi) span")
+        //         .text();
+        //     let Deaths = $(el)
+        //         .find(".text-danger-new1:contains(Tử vong) span")
+        //         .text();
+        //     let dataVn = {
+        //         Confirmed: Confirmed.split(".").join(""),
+        //         Active: Active.split(".").join(""),
+        //         Recovered: Recovered.split(".").join(""),
+        //         Deaths: Deaths.split(".").join(""),
+        //         Country: "Việt Nam",
+        //         Slug: "vietnam",
+        //         ISO2: "vn",
+        //         Date: moment().format("DD-MM-YYYY"),
+        //     };
+
+        //     data.vietnam = dataVn;
+        // });
+
+        // $(selectorWorld).each((index, el) => {
+        //     let Confirmed = $(el).find(".text-danger-new span").text();
+        //     let Active = $(el)
+        //         .find(".text-warning1:contains(Đang nhiễm) span")
+        //         .text();
+        //     let Recovered = $(el)
+        //         .find(".text-success:contains(Khỏi) span")
+        //         .text();
+        //     let Deaths = $(el)
+        //         .find(".text-danger-new1:contains(Tử vong) span")
+        //         .text();
+        //     let dataWolrd = {
+        //         Confirmed: Confirmed.split(".").join(""),
+        //         Active: Active.split(".").join(""),
+        //         Recovered: Recovered.split(".").join(""),
+        //         Deaths: Deaths.split(".").join(""),
+        //         Country: "Thế giới",
+        //         Slug: "world",
+        //         ISO2: "wld",
+        //         Date: moment().format("DD-MM-YYYY"),
+        //     };
+        //     data.world = dataWolrd;
+        // });
+
+        return local_vn;
+    } catch (err) {
+        console.log(
+            errorWaring(
+                `Get MOH source fail because ${err} at ${moment(
+                    new Date()
+                ).format("DD/MM/YYYY HH:MM:SS")}`
+            )
+        );
+    }
+};
+
+const getDetailVN = async () => {
+    try {
+        let res = await axios.default({
+            method: "GET",
+            url: "https://www.worldometers.info/coronavirus/country/viet-nam/",
+            httpsAgent: new (require("https").Agent)({
+                rejectUnauthorized: false,
+            }),
+        });
+
+        console.log(
+            successAlert(
+                `Get VN detail source successfully at ${moment(
+                    new Date()
+                ).format("DD/MM/YYYY HH:mm:SS")}`
+            )
+        );
+
+        let $ = cheerio.load(res.data);
+
+        let selector = $(".maincounter-number");
+        let Confirmed = parseInt($(selector[0]).text().replace(",", ""));
+        let Deaths = parseInt($(selector[1]).text().replace(",", ""));
+        let Recovered = parseInt($(selector[2]).text().replace(",", ""));
+        let Active = Confirmed - Deaths - Recovered;
+        let dataVn = {
+            Confirmed: Confirmed,
+            Active: Active,
+            Recovered: Recovered,
+            Deaths: Deaths,
+            Country: "Việt Nam",
+            Slug: "vietnam",
+            ISO2: "vn",
+            Date: moment().format("DD-MM-YYYY"),
+        };
+
+        return dataVn;
+    } catch (err) {
+        console.log(
+            errorWaring(
+                `Get VN detail source fail because ${err} at ${moment(
+                    new Date()
+                ).format("DD/MM/YYYY HH:MM:SS")}`
+            )
+        );
+    }
+};
+
+const getData = async () => {
+    return new Promise(async (resolve, reject) => {
+        resolve({
+            local_vn: await getLocalVN(),
+            vietnam: await getDetailVN(),
+        });
+    });
+};
+
+module.exports = {
+    getData: getData(),
 };
--- crawl/updateData.js
+++ crawl/updateData.js
@@ -10,65 +10,64 @@ const errorWaring = chalk.bold.red;
 
 const URL = process.env.MONGODB_URL;
 
-module.exports = () => {
-    api.getMohAPI().then((res) => {
-        const { local_vn, vietnam, world } = res;
+module.exports = async () => {
+    const { local_vn, vietnam } = await api.getData;
+    // History.findOneAndUpdate({ Date: world.Date, ISO2: "wld" }, world, {
+    //   upsert: true,
+    //   new: true,
+    //   setDefaultsOnInsert: true,
+    //   useFindAndModify: false,
+    // })
+    //   .then(() => console.log(successAlert(`Saved world history successfully at ${moment(new Date()).format('DD/MM/YYYY HH:mm:SS')}!!!`)))
+    //   .catch(() => console.log(errorWaring(`Saved world history failat ${moment(new Date()).format('DD/MM/YYYY HH:mm:SS')}!!!`)))
 
-        // History.findOneAndUpdate({ Date: world.Date, ISO2: "wld" }, world, {
-        //   upsert: true,
-        //   new: true,
-        //   setDefaultsOnInsert: true,
-        //   useFindAndModify: false,
-        // })
-        //   .then(() => console.log(successAlert(`Saved world history successfully at ${moment(new Date()).format('DD/MM/YYYY HH:mm:SS')}!!!`)))
-        //   .catch(() => console.log(errorWaring(`Saved world history failat ${moment(new Date()).format('DD/MM/YYYY HH:mm:SS')}!!!`)))
-
-        History.findOneAndUpdate({ Date: vietnam.Date, ISO2: "vn" }, vietnam, {
-            upsert: true,
-            new: true,
-            setDefaultsOnInsert: true,
+    // Update daily data
+    History.findOneAndUpdate({ Date: vietnam.Date, ISO2: "vn" }, vietnam, {
+        upsert: true,
+        new: true,
+        setDefaultsOnInsert: true,
+    })
+        .then(() => {
+            console.log(
+                successAlert(
+                    `Saved vietnam history successfully at ${moment().format(
+                        "DD/MM/YYYY HH:mm:SS"
+                    )}!!!`
+                )
+            );
         })
-            .then(() => {
-                console.log(
-                    successAlert(
-                        `Saved vietnam history successfully at ${moment().format(
-                            "DD/MM/YYYY HH:mm:SS"
-                        )}!!!`
-                    )
-                );
-            })
-            .catch(() => {
-                console.log(
-                    errorWaring(
-                        `Saved vietnam history fail at ${moment().format(
-                            "DD/MM/YYYY HH:mm:SS"
-                        )}!!!`
-                    )
-                );
-            });
+        .catch(() => {
+            console.log(
+                errorWaring(
+                    `Saved vietnam history fail at ${moment().format(
+                        "DD/MM/YYYY HH:mm:SS"
+                    )}!!!`
+                )
+            );
+        });
 
-        Local_vn.findOneAndUpdate({ Date: local_vn.Date }, local_vn, {
-            upsert: true,
-            new: true,
-            setDefaultsOnInsert: true,
+    //Update each provines data
+    Local_vn.findOneAndUpdate({ Date: local_vn.Date }, local_vn, {
+        upsert: true,
+        new: true,
+        setDefaultsOnInsert: true,
+    })
+        .then(() => {
+            console.log(
+                successAlert(
+                    `Saved vietnam local successfully at ${moment().format(
+                        "DD/MM/YYYY HH:mm:SS"
+                    )}!!!`
+                )
+            );
         })
-            .then(() => {
-                console.log(
-                    successAlert(
-                        `Saved vietnam local successfully at ${moment().format(
-                            "DD/MM/YYYY HH:mm:SS"
-                        )}!!!`
-                    )
-                );
-            })
-            .catch(() => {
-                console.log(
-                    errorWaring(
-                        `Saved vietnam local fail at ${moment().format(
-                            "DD/MM/YYYY HH:mm:SS"
-                        )}!!!`
-                    )
-                );
-            });
-    });
+        .catch(() => {
+            console.log(
+                errorWaring(
+                    `Saved vietnam local fail at ${moment().format(
+                        "DD/MM/YYYY HH:mm:SS"
+                    )}!!!`
+                )
+            );
+        });
 };
