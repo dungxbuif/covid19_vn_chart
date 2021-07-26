a0a22805210872b2dcacff6869f70dc1142a48c4
DungxBuif <dungbui.dungbui.00@gmail.com>
2021-07-26 20:13:05 +0700

    #3  deploy

--- index.js
+++ index.js
@@ -12,18 +12,6 @@ const port = process.env.PORT || 1612;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(function (req, res, next) {
-   console.log(req.headers);
-   if (
-      req.headers.api_key !== 'MY_TRACKING_COVID' ||
-      req.headers.host !== 'dungxbuif-covid-tracking.herokuapp.com' ||
-      req.headers.referer !== 'https://dungxbuif-covid-tracking.herokuapp.com/'
-   ) {
-      res.json({ errStatus: 'You can not acccess this URL' });
-   }
-
-   next();
-});
 app.use('/public', express.static(path.join(__dirname, 'build/static')));
 app.use('/', express.static(path.join(__dirname, 'build')));
 
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,16 @@ const express = require('express');
 const path = require('path');
 
 module.exports = (app) => {
+   app.use('/api', (req, res, next) => {
+      console.log(req.headers);
+      if (
+         req.headers.api_key !== 'MY_TRACKING_COVID' ||
+         req.headers.host !== 'dungxbuif-covid-tracking.herokuapp.com'
+      ) {
+         res.json({ errStatus: 'You can not acccess this URL' });
+      }
+
+      next();
+   });
    app.use('/api', apiRouter);
 };
