36159a92bb93a07d70bc0d65cb959e1e98d48176
DungxBuif <dungbui.dungbui.00@gmail.com>
2021-08-22 21:57:49 +0700

    fix appi key

--- client/src/apis/index.js
+++
@@ -1,9 +0,0 @@
-import axios from './axiosClient';
-
-export const getDetailVnLocal = () => axios.get('/api/detail-local-vn');
-
-export const getReportByCountry = () => axios.get('/api/history');
-
-export const getReportPerDay = () => axios.get('/api/history-per-day');
-
-export const getVaccines = () => axios.get('/api/vaccine-details');
